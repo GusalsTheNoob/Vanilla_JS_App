010b9746babcd1a5b2b4fa05c5f8e4a598b8de6a
GusalsTheNoob <harrygusals@snu.ac.kr>
2020-05-30 21:53:01 +0900

    0529

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const nicoInfo = {
-  name: "Nico",
-  age: 33,
-  gender: "Male",
-  isHandsome: true,
-};
+const title = document.querySelector("#title");
 
-console.log(nicoInfo);
+const BASE_COLOR = "white";
+const OTHER_COLOR = "blue";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+
+init();
